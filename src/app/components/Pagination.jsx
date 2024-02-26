4a5c067277028c36c53634b7e64702d7b057f12d
Dana Shorts <csudhtoro@hotmail.com>
2024-02-25 18:06:31 -0800

    third commit to pagination

--- src/app/components/Pagination.jsx
+++ src/app/components/Pagination.jsx
@@ -7,27 +7,27 @@ import { GrFormPreviousLink } from "react-icons/gr";
 const Pagination = ({ page, hasPrev, hasNext }) => {
   const router = useRouter();
   return (
-    <div className=" px-4 py-2 flex justify-between w-full">
+    <div className="px-4 py-2 flex justify-between w-full">
       <button
-        className="text-xs px-6 py-4 bg-gray-900 dark:bg-gray-200 text-white dark:text-gray-900 font-semibold disabled:bg-gray-400 dark:disabled:bg-gray-700 disabled:cursor-not-allowed cursor-pointer rounded-lg"
+        className="group text-xs px-6 py-4 bg-gray-900 dark:bg-gray-200 text-white dark:text-gray-900 font-semibold disabled:bg-gray-400 dark:disabled:bg-gray-700 disabled:cursor-not-allowed cursor-pointer rounded-lg"
         disabled={!hasPrev}
         onClick={() => router.push(`?page=${page - 1}`, { scroll: false })}
       >
-        <div className="group flex gap-2 items-center">
+        <div className="flex gap-2 items-center">
           <GrFormPreviousLink
             size={20}
             className="text-white dark:text-gray-900 group-hover:-translate-x-1 transition"
           />
-          Prev
+          <p className="group-hover:scale-105">Prev</p>
         </div>
       </button>
       <button
-        className="text-xs px-6 py-4 bg-gray-900 dark:bg-gray-200 text-white dark:text-gray-900 font-semibold cursor-pointer rounded-lg disabled:bg-gray-400 dark:disabled:bg-gray-700 disabled:cursor-not-allowed"
+        className="group text-xs px-6 py-4 bg-gray-900 dark:bg-gray-200 text-white dark:text-gray-900 font-semibold cursor-pointer rounded-lg disabled:bg-gray-400 dark:disabled:bg-gray-700 disabled:cursor-not-allowed"
         disabled={!hasNext}
         onClick={() => router.push(`?page=${page + 1}`, { scroll: false })}
       >
-        <div className="group flex gap-2 items-center">
-          Next
+        <div className="flex gap-2 items-center">
+          <p className="group-hover:scale-105">Next</p>
           <GrFormNextLink
             size={20}
             className="text-white dark:text-gray-900 group-hover:translate-x-1 transition"
