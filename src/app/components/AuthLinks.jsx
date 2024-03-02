e39b4d5051f1cf5f48e268f73d6a6482550b1259
Dana Shorts <csudhtoro@hotmail.com>
2024-03-01 21:27:15 -0800

    updated hover opacity for cat buttons

--- src/app/components/AuthLinks.jsx
+++ src/app/components/AuthLinks.jsx
@@ -26,7 +26,7 @@ const AuthLinks = () => {
         <>
           <div className="w-full flex justify-between items-center gap-2 sm:gap-5">
             <Link href="/create">
-              <div className="text-xs px-5 py-2 my-3 rounded-lg bg-gray-900 dark:bg-gray-200 text-gray-200 dark:text-gray-900 font-semibold">
+              <div className="text-xs px-5 py-2 my-3 rounded-lg bg-gray-900 dark:bg-gray-200 text-gray-200 dark:text-gray-900 font-semibold hover:opacity-90">
                 Create
               </div>
             </Link>
